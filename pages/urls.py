633cf29d0566fd3930c68976810d05fda6ebfb78
KeganBerg <90342462+KeganBerg@users.noreply.github.com>
2021-12-14 11:29:47 -0500

    Final without Login/Signup

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, ImageGalleryView, SpaceView, NatureView, AbstractView
 
 urlpatterns = [
+    path('abstract.html', AbstractView.as_view(), name='abstract'),
+    path('nature.html', NatureView.as_view(), name='nature'),
+    path('space.html', SpaceView.as_view(), name='space'),
+    path('imagegallery.html', ImageGalleryView.as_view(), name='imagegallery'),
     path('about.html', AboutPageView.as_view(), name='about'),
-    path('', HomePageView.as_view(), name='home'),
+    path('home.html', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,19 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class ImageGalleryView(TemplateView):
+    template_name = 'imagegallery.html'
+
+
+class SpaceView(TemplateView):
+    template_name = 'space.html'
+
+
+class NatureView(TemplateView):
+    template_name = 'nature.html'
+
+
+class AbstractView(TemplateView):
+    template_name = 'abstract.html'
